0da998d81b33327d8d62cdd000cc026d146fc56a
pengyutao <pengyutao@suzhouyunque.com>
2024-06-20 14:52:25 +0800

    解决水合错误

--- src/components/modals/initial-modal.tsx
+++ src/components/modals/initial-modal.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import {useForm} from 'react-hook-form'
-import * as zod from "zod";
+import * as zod from 'zod'
 import {zodResolver} from '@hookform/resolvers/zod'
 
 import {
@@ -12,46 +12,107 @@ import {
   DialogHeader,
   DialogTitle
 } from '@/components/ui/dialog'
+import {
+  Form,
+  FormField,
+  FormControl,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+import {Input} from '@/components/ui/input'
+import {Button} from '@/components/ui/button'
+import {useEffect, useState} from 'react'
 
 const formSchema = zod.object({
   name: zod.string().min(1, {
-    message: "缺少服务器名称"
+    message: '缺少服务器名称'
   }),
   imageUrl: zod.string().min(1, {
-    message: "缺少图片地址"
+    message: '缺少图片地址'
   })
 })
 
+export const InitialModal = () => {
 
+  const [isMounted, setIsMounted] = useState(false)
 
-const InitialModal = () => {
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      name: "",
-      imageUrl: ""
+      name: '',
+      imageUrl: ''
     }
   })
 
   const isLoading = form.formState.isSubmitting
+
   const onSubmit = async (values: zod.infer<typeof formSchema>) => {
     console.log(values)
   }
 
+  if (!isMounted) {
+    return null;
+  }
+
   return (
-   <Dialog open>
-     <DialogContent className='bg-white text-black p-0 overflow-hidden'>
-       <DialogHeader className='pt-8 px-6'>
-         <DialogTitle className='text-2xl text-center font-bold'>
-           Customize your server
-         </DialogTitle>
-         <DialogDescription className='text-center text-zinc-500'>
-           你好
-         </DialogDescription>
-       </DialogHeader>
-     </DialogContent>
-   </Dialog>
+    <Dialog open>
+      <DialogContent className='bg-white text-black p-0 overflow-hidden'>
+        <DialogHeader className='pt-8 px-6'>
+          <DialogTitle className='text-2xl text-center font-bold'>
+            Customize your server
+          </DialogTitle>
+          <DialogDescription className='text-center text-zinc-500'>
+            你好
+          </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}
+                className='space-y-8'>
+            <div className='space-y-8 px-6'>
+              <div className='flex items-center justify-center text-center'>
+                TODO: Image Upload
+              </div>
+
+              <FormField
+                control={form.control}
+                name='name'
+                render={({field}) => (
+                  <FormItem>
+                    <FormLabel
+                      className='uppercase text-xs font-bold text-zinc-500
+                    dark:text-secondary/70'
+                    >
+                      Server name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className='bg-zinc-300/50 border-0
+                        focus-visible:ring-0 text-black
+                        focus-visible:ring-offset-0'
+                        placeholder='Enter server name'
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className='bg-gray-100 px-6 py-4'>
+              <Button variant='primary' disabled={isLoading}>
+                创建
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
   )
 }
-
-export default InitialModal
