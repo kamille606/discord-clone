76f07ff52e5690d950ebb74e94800f9ce8e607d7
pengyutao <pengyutao@suzhouyunque.com>
2024-06-21 18:04:09 +0800

    server主页

--- src/components/navigation/navigation-sidebar.tsx
+++ src/components/navigation/navigation-sidebar.tsx
@@ -1,5 +1,7 @@
 import {redirect} from 'next/navigation'
+import {UserButton} from '@clerk/nextjs'
 
+import {ModeToggle} from '@/components/mode-toggle'
 import {Separator} from '@/components/ui/separator'
 import {ScrollArea} from '@/components/ui/scroll-area'
 import {NavigationAction} from '@/components/navigation/navigation-action'
@@ -45,6 +47,17 @@ export const NavigationSidebar = async () => {
           </div>
         ))}
       </ScrollArea>
+      <div className='pb-3 mt-auto flex items-center flex-col gap-y-4'>
+        <ModeToggle/>
+        <UserButton
+          afterSignOutUrl='/'
+          appearance={{
+            elements: {
+              avatarBox: 'h-[48px] w-[48px]'
+            }
+          }}
+        />
+      </div>
     </div>
   )
 }
